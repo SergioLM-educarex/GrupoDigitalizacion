f7b6ef7f0c68072b4835c7bb48f89c242f3f7585
smolanoh01 <smolanoh01@educarex.es>
2025-12-03 13:26:40 +0100

    Commit de Sergio Molano

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Proyecto base");
+		
+		if (clase_SergioMolano.validarDNI("49837823C") == true) {
+			System.out.println("DNI correto.");
+		} else {
+			System.out.println("DNI erróneo.");
+		}
 	}
 
 }
